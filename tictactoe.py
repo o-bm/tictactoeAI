74e382af246d0d789b84a5f728376c937eca74b1
Omar <iiobm127@gmail.com>
2023-05-23 08:53:59 +0300

    made some changes

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,9 @@ X = "X"
 O = "O"
 EMPTY = None
 
+class InvalidAction(Exception):
+    "Raise exception for invalid action"
+    pass
 
 def initial_state():
     """
@@ -22,14 +25,30 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    x_counter = 0
+    o_counter = 0
+    for row in board:
+        for item in row:
+            if item == X:
+                x_counter += 1
+            elif item == O:
+                o_counter += 1
+    if x_counter > o_counter:
+        return o_counter
+    else:
+        return x_counter
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    moves_set = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                moves_set.add((i,j))
+    return moves_set
 
 
 def result(board, action):
